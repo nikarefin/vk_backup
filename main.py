6cf1a11d82fc2dafc326877c09e4fa0e381bfcd1
Nik Arefin <nikarefin@gmail.com>
2023-10-05 00:37:51 +0300

    Убран поход методов за данными в другие классы; добавлено пошаговое получение токена

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
 import requests
 import json
 import time
+from colorama import Fore, Style
+from urllib.parse import urlencode
 from tqdm import tqdm
-
-VK_TOKEN = ''
-
-class Start:
-    vk_id = input('VK ID: ')
-    ya_token = input('Я.Токен: ')
 
 
 class VKClient:
+    APP_ID = '51755696'
     API_BASE_URL = 'https://api.vk.com/method'
+    OAUTH_BASE_URL = 'https://oauth.vk.com/authorize'
     photo_urls = []
 
-    def __init__(self, token):
-        self.token = token
+    def __init__(self):
+        self.vk_token = None
+        self.vk_id = None
+
+    def get_token(self):
+        params = {
+            'client_id': self.APP_ID,
+            'redirect_uri': 'https://oauth.vk.com/blank.html',
+            'display': 'page',
+            'scope': 'photos',
+            'response_type': 'token'
+        }
+        outh_link = f'{self.OAUTH_BASE_URL}?{urlencode(params)}'
+        print(f'1. Перейдите по ссылке: {outh_link}')
+        print('2. Подтвердите, что хотите передавать ваши данные приложению.')
+        print('3. Скопируйте адрес открывшейся страницы из адресной строки браузера.')
+        token_address = input('4. Вставьте скопированный адрес сюда и нажмите Enter: ')
+        self.vk_token = token_address[
+                   token_address.index('=') + 1:token_address.index('&')
+                        ]
+        return self.vk_token
+
+    def ask_vk_id(self):
+        self.vk_id = input('5. Укажите ID профиля, из которого брать фото: ')
+        return self.vk_id
 
     def get_common_params(self):
         return {
-            'access_token': self.token,
+            'access_token': self.vk_token,
             'v': '5.131'
         }
 
     def get_photos(self):
-        print('\nПолучаем фотографии из VK:')
+        print(f'{Fore.BLUE}\nСтартуем', end='')
+        for i in range(5):
+            time.sleep(0.5)
+            print('.', end='')
+        print(f'{Style.RESET_ALL}\n\nПолучаем фотографии из VK:')
         photos = []
         same_names = []
         params = self.get_common_params()
         params.update(
             {
-                'owner_id': Start.vk_id,
+                'owner_id': self.vk_id,
                 'album_id': 'profile',
                 'extended': '1'
             }
@@ -53,22 +78,28 @@ class VKClient:
             time.sleep(.05)
         with open('photos.json', 'w') as file:
             json.dump(photos, file, indent=2)
-        print('Готово!')
+        print(f'{Fore.BLUE}Готово!{Style.RESET_ALL}')
 
 
 class YADisk:
     BASE_URL = 'https://cloud-api.yandex.net'
     CREATING_FOLDER_URL = f'{BASE_URL}/v1/disk/resources'
     UPLOADING_URL = f'{CREATING_FOLDER_URL}/upload'
-    UPLOADING_FOLDER = 'VK Backups'
 
-    def upload_photos(self):
+    def __init__(self):
+        self.ya_token = None
+
+    def ask_ya_token(self):
+        self.ya_token = input('6. Введите ваш Я.Токен: ')
+        return self.ya_token
+
+    def upload_photos(self, upload_folder='VK Backup'):
         print('\nЗагружаем фотографии на Я.Диск:')
         headers = {
-            'Authorization': Start.ya_token
+            'Authorization': self.ya_token
         }
         folder_params = {
-            'path': self.UPLOADING_FOLDER
+            'path': upload_folder
         }
         requests.put(
             self.CREATING_FOLDER_URL, headers=headers, params=folder_params
@@ -81,19 +112,23 @@ class YADisk:
 
         for photo in tqdm(photos):
             upload_params = {
-                'path': f'{self.UPLOADING_FOLDER}/{photo["file_name"]}',
+                'path': f'{upload_folder}/{photo["file_name"]}',
                 'url': photo['url']
             }
             requests.post(
                 self.UPLOADING_URL, headers=headers, params=upload_params
             )
             time.sleep(.05)
-        print('Готово!')
+        print(f'{Fore.BLUE}Готово!{Style.RESET_ALL}')
+        print(f'{Fore.LIGHTGREEN_EX}\nФотографии успешно загружены '
+              f'на Я.Диск в папку {upload_folder}{Style.RESET_ALL}')
 
 
-if __name__ == '__main__':
-    s = Start()
-    vk = VKClient(VK_TOKEN)
-    vk.get_photos()
-    ya = YADisk()
-    ya.upload_photos()
+print('VK BACKUP\n')
+vk = VKClient()
+ya = YADisk()
+vk.get_token()
+vk.ask_vk_id()
+ya.ask_ya_token()
+vk.get_photos()
+ya.upload_photos()
